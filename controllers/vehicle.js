e980ceadba6a5e7397415920128f5c49be83440e
nandinikandi <77817444+nandinikandi@users.noreply.github.com>
2021-04-06 12:24:46 -0500

    Update vehicle.js
    modified vehicle_list (get list of all vehicles)

--- controllers/vehicle.js
+++ controllers/vehicle.js
@@ -1,21 +1,28 @@
 var Vehicle = require('../models/vehicle');
-// List of all Vehicle
-exports.vehicle_list = function(req, res) {
-res.send('NOT IMPLEMENTED: Vehicle list');
+// List of all Vehicles
+exports.vehicle_list = async function (req, res) {
+    try {
+        theVehicles = await Vehicle.find();
+        res.send(theVehicles);
+    }
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
 };
 // for a specific Vehicle.
-exports.vehicle_detail = function(req, res) {
-res.send('NOT IMPLEMENTED: Vehicle detail: ' + req.params.id);
+exports.vehicle_detail = function (req, res) {
+    res.send('NOT IMPLEMENTED: Vehicle detail: ' + req.params.id);
 };
 // Handle Vehicle create on POST.
-exports.vehicle_create_post = function(req, res) {
-res.send('NOT IMPLEMENTED: Vehicle create POST');
+exports.vehicle_create_post = function (req, res) {
+    res.send('NOT IMPLEMENTED: Vehicle create POST');
 };
 // Handle Vehicle delete form on DELETE.
-exports.vehicle_delete = function(req, res) {
-res.send('NOT IMPLEMENTED: Vehicle delete DELETE ' + req.params.id);
+exports.vehicle_delete = function (req, res) {
+    res.send('NOT IMPLEMENTED: Vehicle delete DELETE ' + req.params.id);
 };
 // Handle Vehicle update form on PUT.
-exports.vehicle_update_put = function(req, res) {
-res.send('NOT IMPLEMENTED: Vehicle update PUT' + req.params.id);
+exports.vehicle_update_put = function (req, res) {
+    res.send('NOT IMPLEMENTED: Vehicle update PUT' + req.params.id);
 };

6fae6392e7195c49839a9950e190bd6d3d09b90f
nandinikandi <77817444+nandinikandi@users.noreply.github.com>
2021-04-23 14:04:15 -0500

    adding buttons to the collections page and BONUS
    add create, view and delete button

--- models/vehicle.js
+++ models/vehicle.js
@@ -1,7 +1,20 @@
 const mongoose = require("mongoose")
 const vehicleSchema = mongoose.Schema({
-make: String,
-model: String,
-year: Number
+make: {
+    type: String,
+    minLength: 3,
+    maxLength: 10,
+    required: [true, 'Vehicle Make is required']
+  },
+model: {
+    type: String,
+    minLength: 3,
+    required: [true, 'Vehicle Model is required']
+  },
+year: {
+    type: Number,
+    min: [1950, 'Too Low'],
+    max: [2050, 'Too High']
+}
 })
 module.exports = mongoose.model("Vehicle", vehicleSchema)
--- routes/vehicle.js
+++ routes/vehicle.js
@@ -22,13 +22,13 @@ router.get('/', vehicle_controlers.vehicle_view_all_Page);
 router.get('/detail', vehicle_controlers.vehicle_view_one_Page);
 
 /* GET create vehicle page */
-router.get('/create', vehicle_controlers.vehicle_create_Page);
+router.get('/create', secured, vehicle_controlers.vehicle_create_Page);
 
 /* GET create update page */
 router.get('/update', secured, vehicle_controlers.vehicle_update_Page);
 
 /* GET create vehicle page */
-router.get('/delete', vehicle_controlers.vehicle_delete_Page);
+router.get('/delete', secured, vehicle_controlers.vehicle_delete_Page);
 
 
 // router.get('/', function(req, res, next) {
